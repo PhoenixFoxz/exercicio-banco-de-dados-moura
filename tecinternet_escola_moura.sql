646bf2ed2cbb33d33a6231dfd0b79765140760cf
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2023-08-21 09:15:50 -0300

    Correção da atividade

--- tecinternet_escola_moura.sql
+++ tecinternet_escola_moura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/08/2023 às 13:48
+-- Tempo de geração: 21/08/2023 às 14:15
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -58,7 +58,7 @@ CREATE TABLE `cursos` (
 CREATE TABLE `professores` (
   `id` smallint(6) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `atuacao` varchar(45) NOT NULL,
+  `atuacao` enum('design','desenvolvimento','infra') NOT NULL,
   `curso_id` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
